f2c12f2dd5eb0d45ae750b9d8b454ae26602ca7e
Daniel Miller (Earthmark) <earthmark.miller@gmail.com>
2020-07-21 18:33:30 -0700

    Added initial project files

---
+++ LockPatron/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using LockPatron.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LockPatron.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class WeatherForecastController : ControllerBase
+  {
+    private static readonly string[] Summaries =
+    {
+      "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly ForecastContext _ctx;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, ForecastContext ctx)
+    {
+      _logger = logger;
+      _ctx = ctx;
+    }
+
+    [HttpGet]
+    public IQueryable<WeatherForecast> Get([FromQuery, Range(0, 50)] int count)
+    {
+      return _ctx.Forecasts.Take(count);
+    }
+
+    [HttpPost]
+    public async Task<WeatherForecast> Post()
+    {
+      var rng = new Random();
+      var entity = new WeatherForecast
+      {
+        Date = DateTime.Now.AddDays(rng.Next(0, 30)),
+        TemperatureC = rng.Next(-20, 55),
+        Summary = Summaries[rng.Next(Summaries.Length)]
+      };
+      _ctx.Forecasts.Add(entity);
+      await _ctx.SaveChangesAsync(HttpContext.RequestAborted);
+      return entity;
+    }
+  }
+}
---
+++ LockPatron/Model/ForecastContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LockPatron.Model
+{
+  public class ForecastContext : DbContext
+  {
+    public ForecastContext(DbContextOptions<ForecastContext> options) : base(options)
+    {
+    }
+
+    public DbSet<WeatherForecast> Forecasts { get; set; }
+  }
+}
